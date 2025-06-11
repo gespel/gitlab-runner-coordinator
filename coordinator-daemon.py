4b990abafc50db64f4a79ba910e5c8d882a726d1
gespel <sten.heimbrodt@hotmail.de>
2025-06-11 11:32:27 +0200

    working sample

--- coordinator-daemon.py
+++ coordinator-daemon.py
@@ -1,18 +1,39 @@
 import requests
 import os
 
-api_token = os.environ["GITLAB_API_TOKEN"]
+class CoordinatorDaemon:
+    def __init__(self):
+        self.api_token = os.environ["GITLAB_API_TOKEN"]
+        self.headers = {
+            "PRIVATE-TOKEN": self.api_token
+        }
+        self.runners = []
 
-headers = {
-    "PRIVATE-TOKEN": api_token
-}
-url = "https://gitlab.com/api/v4/projects/70726275/runners"
+    def fetch_runners(self):
+        response = requests.get("https://gitlab.com/api/v4/projects/70726275/runners", headers=self.headers)
 
-response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            runners = response.json()
+            for runner in runners:
+                print(runner)
+                self.runners.append(runner)
 
-if response.status_code == 200:
-    runners = response.json()
-    for runner in runners:
-        print(f"Runner ID: {runner['id']} - Description: {runner.get('description')}")
-else:
-    print(f"Fehler: {response.status_code} - {response.text}")
+        else:
+            print(f"Fehler: {response.status_code} - {response.text}")
+
+    def get_num_jobs_from_runner(self, runner_id):
+        response = requests.get(f"https://gitlab.com/api/v4/runners/{runner_id}/jobs", headers=self.headers)
+        out = []
+        if response.status_code == 200:
+            jobs = response.json()
+            out = jobs
+        else:
+            print(f"Fehler: {response.status_code} - {response.text}")
+        return out
+
+
+cd = CoordinatorDaemon()
+cd.fetch_runners()
+for runner in cd.runners:
+    jobs = cd.get_num_jobs_from_runner(runner["id"])
+    print(f"Current number of jobs on {runner["id"]}: {len(jobs)}")
