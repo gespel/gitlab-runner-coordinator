79580ae4128d14691703bc7dc65860326af62e9c
gespel <sten.heimbrodt@hotmail.de>
2025-06-20 09:23:48 +0200

    shorter print statement for shutdown function

--- coordinator-daemon.py
+++ coordinator-daemon.py
@@ -55,8 +55,7 @@ class CoordinatorDaemon:
     def shutdown_vm(self, runner_id):
         import subprocess
         out = subprocess.run(["gcloud", "compute", "instances", "stop", self.get_vm_name(runner_id), "--zone=europe-west3-a"])
-        out = str(out)
-        print(out)
+        print(str(out))
 
 
 cd = CoordinatorDaemon()
